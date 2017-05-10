a20d0a313d9923807dc7dffb851c2c432d6ef228
sunita <sunitadjondhale@gmail.com>
2017-05-11 04:39:05 +0530

    Using uixml convert lowecase into uppercase

--- uixml/src/main/java/com/example/uixml/MainActivity.java
+++ uixml/src/main/java/com/example/uixml/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.uixml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText txt1=(EditText)findViewById(R.id.editName);
+        final Button but=(Button)findViewById(R.id.ButnName);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = txt1.getText().toString();
+                txt1.setText(str.toUpperCase());
+            }
+        });
     }
 }
