137aa3c5f38d06933d603d27aef126e91b99c558
sunita <sunitadjondhale@gmail.com>
2017-05-11 14:11:38 +0530

    RelativeLayout

--- uixml/src/main/java/com/example/uixml/MainActivity.java
+++ uixml/src/main/java/com/example/uixml/MainActivity.java
@@ -6,9 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
+public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
